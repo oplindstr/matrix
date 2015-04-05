b4bd9593cf6ec509bba362d3243b17a0f211a190
oplindstr <olli-pekka.lindstrom@helsinki.fi>
2015-04-05 17:07:46 +0300

    huutopussikorjauksia

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -72,7 +72,6 @@ class MatchesController < ApplicationController
     params["match"]["hands_attributes"].permit!
     hands = params["match"]["hands_attributes"]
     @winners = params["winners"];
-    qui
     @match_id = params["match"]["hands_attributes"].first[1]["match_id"]
     if @match_id.nil?
       redirect_to jatkantappajat_path
