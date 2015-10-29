26c32f40b6906831e2ef28234113f77d1f2b2a37
oplindstr <olli-pekka.lindstrom@helsinki.fi>
2015-10-29 19:30:41 +0200

    migration change

--- db/migrate/20150910155800_add_descr_to_stringresource.rb
+++
@@ -1,5 +0,0 @@
-class AddDescrToStringresource < ActiveRecord::Migration
-  def change
-  	add_column :stringresources, :descr, :string
-  end
-end
--- db/migrate/20151029190700_drop_settings.rb
+++
@@ -1,6 +0,0 @@
-class DropSettings < ActiveRecord::Migration
-  def change
-  	drop_table(:stringresources)
-  	drop_table(:settings)
-  end
-end
