600acb8a20feeb0e7afec5649138368322a9e935
Olli-Pekka Lindström <olli-pekka.lindstrom@helsinki.fi>
2016-03-23 16:14:54 +0200

    blogin aloitus

--- db/migrate/201603031338_remove_image_stuff.rb
+++ db/migrate/201603031338_remove_image_stuff.rb
@@ -1,9 +1,6 @@
 class RemoveImageStuff < ActiveRecord::Migration
   def change
-  	remove_column :board_members, :avatar_file_name
-  	remove_column :board_members, :avatar_content_type
-  	remove_column :board_members, :avatar_file_size
-  	remove_column :board_members, :avatar_updated_at
+  	remove_column :board_members, :avatar
   	remove_column :board_members, :attachment
   end
 end
