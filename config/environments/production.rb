47db4566a50736b486f9eff7c906f856cd66692b
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2017-07-13 18:33:52 +0300

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
