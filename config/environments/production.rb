7c91fac97b1ca46f984bff3a792b271c6a24058f
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2017-12-26 21:40:20 +0200

    deploy update test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
+  config.secret_token = ENV['SECRET_TOKEN']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
