0f6d0cfa8cb6fb082569a655eacd2e0675db8e30
Olli-Pekka Lindstrom <olli-pekka.lindstrom@helsinki.fi>
2018-01-10 07:38:41 +0200

    fix

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
    belongs_to :document_group
 
    validates :name, presence: { message: 'Nimi puuttuu' }
-   validates :name, length: { maximum: 500, 'Anna nimi, jonka pituus on enintään 500 merkkiä' }
+   validates :name, length: { maximum: 500, message: 'Anna nimi, jonka pituus on enintään 500 merkkiä' }
 
    mount_uploader :attachment, DocumentUploader
    validates :attachment, presence: { message: 'Tiedosto puuttuu' }
