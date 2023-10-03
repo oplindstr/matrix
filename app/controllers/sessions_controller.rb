8a2a54f7ef4c11ed73023e38fb73a21a50f78270
Olli-Pekka Lindström <olli-pekkalindstrom@hotmail.com>
2023-10-03 22:02:07 +0300

    Fixed redirect back handling after rails version update. Added login error translations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,14 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by username: params[:username]
     if session[:user_id]
-      redirect_to :back, alert: "Olet jo kirjautunut sisään"
+      redirect_back(fallback_location: root_path, alert: I18n.t('already_logged_in'))
     elsif @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to session.delete(:return_to), notice: "#{I18n.t('Welcome')} #{@user.firstname}!"
     elsif @user && !@user.authenticate(params[:password])
-      redirect_to :back, alert: "Salasana väärin"
+      redirect_back(fallback_location: root_path, alert: I18n.t('wrong_password'))
     elsif !@user
-      redirect_to :back, alert: "Käyttäjää #{params[:username]} ei ole!"
+      redirect_back(fallback_location: root_path, alert: I18n.t('user_doesnt_exist'))
     end
   end
 
