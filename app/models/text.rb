9e866f0daee649e00b9cc24d2c4deff9262e25f5
Olli-Pekka Lindström <olli-pekkalindstrom@hotmail.com>
2023-10-03 22:08:38 +0300

    Show text translation only if it is not empty

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,6 @@
 class Text < ActiveRecord::Base
     def get_value
-        if I18n.locale == :en and self.value_eng
+        if I18n.locale == :en and self.value_eng and self.value_eng.length > 0
             return self.value_eng
         end
         return self.value
